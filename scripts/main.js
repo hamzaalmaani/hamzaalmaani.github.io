52c672700d7561d5c61eb4ec408e911aeefb397e
hamzaalmaani <92943723+hamzaalmaani@users.noreply.github.com>
2021-10-22 23:12:07 +0300

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -2,27 +2,27 @@ let myImage = document.querySelector("img");
 
 myImage.onclick = function () {
   let mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/firefox-icon.png") {
-    myImage.setAttribute("src", "https://www.fillmurray.com/256/256");
+  if (mySrc === "images/photo1.jpg") {
+    myImage.setAttribute("src", "images/photo3.jpg");
   } else {
-    myImage.setAttribute("src", "images/firefox-icon.png");
+    myImage.setAttribute("src", "images/photo1.jpg");
   }
 };
 let myButton = document.querySelector("button");
 let myHeading = document.querySelector("h1");
 function setUserName() {
-  let myName = prompt("Please enter your name.");
+  let myName = prompt("Enter one of this:1-My Sweet Heart 2-My Soul");
   if (!myName) setUserName();
   else {
     localStorage.setItem("name", myName);
-    myHeading.textContent = "Mozilla is cool, " + myName;
+    myHeading.textContent = myName + " Rahaf, ";
   }
 }
 if (!localStorage.getItem("name")) {
   setUserName();
 } else {
   let storedName = localStorage.getItem("name");
-  myHeading.textContent = "Mozilla is cool, " + storedName;
+  myHeading.textContent = "hi, " + storedName;
 }
 myButton.onclick = function () {
   setUserName();
